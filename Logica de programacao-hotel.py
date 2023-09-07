76cf66c5938cd8c9e8301f6059bc4debe45b3374
Laila <lai.m.taha@gmail.com>
2023-09-07 12:16:45 -0300

    add push

--- Logica de programacao-hotel.py
+++ Logica de programacao-hotel.py
@@ -1,4 +1,3 @@
-#for
 for i in range (0, 20, 1):
     if(i == 13):
         continue
@@ -9,10 +8,10 @@ for i in range (0, 20, 1):
 #While
 numero=20
 
-while i >=1:
+while numero>=1:
     if (numero == 13):
         numero -=1
         continue
     else:
-        print (i+1)
+        print (numero)
         numero -=1
